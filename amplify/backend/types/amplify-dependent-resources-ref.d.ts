30e19bbd4bd2f18a0de20c9111a68bca40bddf85
Andrii Noha <ajlviv@gmail.com>
2022-03-22 14:46:04 +0200

    amplify push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "vueamplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
