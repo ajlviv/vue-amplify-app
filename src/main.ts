edce9feb19a490b834fb5498bf1bfae13993bd0c
Andrii Noha <ajlviv@gmail.com>
2022-03-22 14:21:46 +0200

    fix main import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import { Amplify } from 'aws-amplify'
 import AmplifyVue from '@aws-amplify/ui-vue'
-import awsExports from './aws-exports'
+import awsExports from '@/aws-exports'
 Amplify.configure(awsExports)
 
 createApp(App).use(AmplifyVue).mount('#app')
